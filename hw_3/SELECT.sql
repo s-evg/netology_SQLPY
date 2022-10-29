518a334fb382b0a1469c6c45c017909ae20f9037
s-evg <s-evg@bk.ru>
2022-10-29 14:12:14 +0500

    Home work 3

--- hw_3/SELECT.sql
+++ hw_3/SELECT.sql
@@ -1,18 +1,18 @@
-SELECT album_name, album_date FROM albums    
-	WHERE album_date = 2018;
+SELECT name, year FROM album    
+	WHERE year = 2018;
 
-SELECT track_name, track_duration FROM tracks t    
-	ORDER BY track_duration DESC
+SELECT title, duration FROM track    
+	ORDER BY duration DESC
 	LIMIT 1;
 	
-SELECT track_name FROM tracks 
-	WHERE track_duration >= 210;
+SELECT title FROM track 
+	WHERE duration >= 210;
 	
-SELECT collection_name FROM collections 
-	WHERE collection_date BETWEEN 2018 and 2020;
+SELECT title FROM collection 
+	WHERE year BETWEEN 2018 and 2020;
 	
-SELECT artist_name FROM artists
-	WHERE artist_name NOT LIKE '%% %%';
+SELECT name FROM artist
+	WHERE name NOT LIKE '%% %%';
 	
-SELECT track_name FROM tracks
-	WHERE track_name LIKE '%%My%%';
+SELECT title FROM track
+	WHERE title LIKE '%%My%%';
